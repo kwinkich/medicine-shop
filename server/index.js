173bd05176bc519903553ae89f89add5d72f2d4e
kwinkich <92987991+kwinkich@users.noreply.github.com>
2024-03-28 16:19:08 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 const PORT = process.env.PORT || '5000';
 const DB_URL =
 	process.env.DB_URL ||
-	'mongodb+srv://root:root@cluster0.7g6zgrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+	'your url';
 const app = express();
 app.use(cors());
 app.use(express.json());
